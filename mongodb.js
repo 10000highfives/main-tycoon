50c787a85aa82db67873bc55e8b2bbcffd1f7982
10000highfives <jared.a.fowler@gmail.com>
2016-02-23 20:51:38 -0800

    adds user schema and saves dummy data

--- mongodb.js
+++ mongodb.js
@@ -1,23 +1,49 @@
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 const mongoURI = 'mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api';
 
 mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
 
+var UserSchema = new Schema({
+	sessionId: Number,
+	authToken: Number,
+	apiLink: [
+		{
+			date: Date,
+			url: String
+		}
+	]
+});
+
+var UserModel = mongoose.model('User', UserSchema);
+
+var session = 234234;
+var atoken = 23459082347345;
+
+function addData (url) {
+	var user = new UserModel();
+	user.sessionId = session;
+	user.authToken = atoken;
+	console.log('loggin inside addData');
+	if (user.apiLink) {
+		user.apiLink.push({
+			date: new Date(),
+			url: url
+		});
+	}
+	user.save((err) => {
+		if (err) throw new Error ('error on write to db', err);
+	});
+}
+
+addData('http://www.google.com')
+addData('http://www.reddit.com')
+addData('http://www.facebook.com')
 
 
-// MongoClient.connect('mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api', function(err, db) {
-//   db.createCollection('api-collection', { validator:
-//     { $or: {
-//       apiURL: { $type: 'string'},
-//       }
-//     }
-//   }, function(err, result) {
-//     db.close();
-//   });
-// });
 
-module.exports = MongoClient.connect.bind(null, 'mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api');
+
+module.exports = UserModel;
