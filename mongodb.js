c8184274d7286bc18f0f38f41ec09fbdf997b08a
10000highfives <jared.a.fowler@gmail.com>
2016-02-23 19:59:46 -0800

    starts setup for creating schema with mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,14 +1,23 @@
 var MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-MongoClient.connect('mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api', function(err, db) {
-  db.createCollection('api-collection', { validator:
-    { $or: {
-      apiURL: { $type: 'string'},
-      }
-    }
-  }, function(err, result) {
-    db.close();
-  });
-});
+const mongoURI = 'mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api';
+
+mongoose.connect(mongoURI);
+
+const db = mongoose.connection;
+
+
+
+// MongoClient.connect('mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api', function(err, db) {
+//   db.createCollection('api-collection', { validator:
+//     { $or: {
+//       apiURL: { $type: 'string'},
+//       }
+//     }
+//   }, function(err, result) {
+//     db.close();
+//   });
+// });
 
 module.exports = MongoClient.connect.bind(null, 'mongodb://team-big-gulp:biggulp14@ds015478.mongolab.com:15478/big-gulp-api');
